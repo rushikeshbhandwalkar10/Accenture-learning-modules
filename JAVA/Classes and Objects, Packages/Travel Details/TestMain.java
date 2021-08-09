2f45e9c56fc0c2eb91a8b6b6dda70c5a2581fd70
rushikeshbhandwalkar10 <54694355+rushikeshbhandwalkar10@users.noreply.github.com>
2021-08-09 11:19:11 +0530

    Update TestMain.java

--- JAVA/Classes and Objects, Packages/Travel Details/TestMain.java
+++ JAVA/Classes and Objects, Packages/Travel Details/TestMain.java
@@ -1,60 +1,32 @@
 import java.util.*;
-
-public class TestMain{
-    
+public class TestMain
+{
     public static BusTicket getTicketDetails()
     {
-        Scanner sc = new Scanner(System.in);
-        BusTicket bt = new BusTicket();
-        
-        System.out.println("Enter the passenger name:");
-        String name = sc.nextLine();
-        System.out.println("Enter the gender(M or F / m or f):");
-        char gender = sc.next().charAt(0);
-        System.out.println("Enter the age:");
-        int age=sc.nextInt();
-        sc.nextLine();
-        
-        Person p = new Person();
-        p.setName(name);
-        p.setGender(gender);
-        p.setAge(age);
-        bt.setPerson(p);
-        
+        BusTicket bt= new BusTicket();
+        Scanner sc= new Scanner(System.in);
         System.out.println("Enter the ticket no:");
-        int ticketNo = sc.nextInt();
-        bt.setTicketNo(ticketNo);
-        sc.nextLine();
+        bt.setTicketNo(sc.nextInt());
         System.out.println("Enter the ticket price:");
-        float ticketprice = sc.nextFloat();
-        bt.setTicketPrice(ticketprice);
-        
+        bt.setTicketPrice(sc.nextFloat());
         return bt;
     }
-    
-    public static void main(String[] args)
-    {
-        TestMain tm = new TestMain();
-        BusTicket bt_1 = new BusTicket();
-        
-        bt_1 = tm.getTicketDetails();
-        bt_1.calculateTotal();
-        System.out.println("Ticket no:"+bt_1.getTicketNo());
-        bt_1.getPerson();
-        System.out.println("Price of a ticket :"+bt_1.getTicketPrice());
-        System.out.println("Total Amount :"+bt_1.getTotalAmount());
+    public static void main (String[] args) {
+        Scanner sc= new Scanner(System.in);
+        Person p= new Person();
+        System.out.println("Enter the passenger name:");
+        p.setName(sc.nextLine());
+        System.out.println("Enter the gender(M or F / m or f):");
+        p.setGender(sc.next().charAt(0));
+        System.out.println("Enter the age:");
+        p.setAge(sc.nextInt());
+        BusTicket bt= getTicketDetails();
+        bt.setPerson(p);
+        p= bt.getPerson();
+        System.out.println("Ticket no:"+bt.getTicketNo());
+        System.out.println("Passenger Name:"+p.getName());
+        System.out.println("Price of a ticket : "+bt.getTicketPrice());
+        bt.calculateTotal();
+        System.out.println("Total Amount : "+bt.getTotalAmount());
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+} 
